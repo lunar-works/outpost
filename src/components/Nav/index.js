c1ee545690b922fa8c54116b0376c09cda459fb6
Anthony Totton <novocast7@gmail.com>
2019-01-27 12:14:40 +0000

    dashboard nav rework

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,15 +1,20 @@
 import React, { PureComponent } from 'react'
+import { NavLink } from "react-router-dom"
 
 class Nav extends PureComponent {
   constructor(props) {
     super(props)
   }
 
-
   render() {
+    const items = this.props.items
     return (
       <nav>
-        { this.props.children }
+      { items.map(item => {
+        return (
+          <NavLink to={ "browse/" + item.alias}>{item.title}</NavLink>
+        )
+      } )}
       </nav>
     )
   }
