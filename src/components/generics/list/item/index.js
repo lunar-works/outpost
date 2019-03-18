1306beb1910a3effeb83446e55b452712194a434
Anthony Totton <novocast7@gmail.com>
2019-03-18 17:45:42 +0000

    comples refactor to less classes. much delete

--- src/components/generics/list/item/index.js
+++ src/components/generics/list/item/index.js
@@ -1,13 +1,18 @@
 import React, { Component } from 'react'
 import styled from 'styled-components'
 
-const Wrap = styled.div`
+const ListWrap = styled.div`
     display: flex;
     position: relative;
 `
-
-const Field = styled.div`
+const ListField = styled.div`
     flex: 1;
+`
+const TableWrap = styled.tr`
+
+`
+const TableField = styled.td`
+
 `
 
 class Item extends Component  {
@@ -23,10 +28,9 @@ class Item extends Component  {
 
     renderActions() {
         const {actions = [], id} = this.props
-        console.log(actions)
-        if(actions.length > 0) {
+        if (actions.length > 0) {
             return(
-                <div >
+                <div>
                     { actions.map((action) => {
 
                         if (action === 'edit') {
@@ -41,13 +45,22 @@ class Item extends Component  {
     }
 
     renderField({label, value, type, editable}, key) {
+        const {tables} = this.props
+        let Field = ListField
+        if (tables === true) {
+            Field = TableField
+        }
         return (
             <Field>{ value }</Field>
         )
     }
 
     render() {
-        const {item, id} = this.props
+        const {item, id, tables} = this.props
+        let Wrap = ListWrap
+        if (tables === true) {
+            Wrap = TableWrap
+        }
         return (
             <Wrap>
                 { item.map((field) => this.renderField(field, id)) }
